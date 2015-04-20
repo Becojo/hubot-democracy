4e10346d0bda5f8a4e17322415b7e0feed294489
Benoit Cote-Jodoin <benoit.cote.jodoin@gmail.com>
2015-04-20 09:32:07 -0400

    added votes and results

--- src/hubot-democracy/core.cljs
+++ src/hubot-democracy/core.cljs
@@ -19,19 +19,33 @@
   (swap! brain assoc key val))
 
 
-
 (defn poll-active? [robot]
   (not (nil? (brain-get robot :question))))
 
 
 (defn display-choices [choices]
   (str/join "\n"
-          (map-indexed (fn [i choice]
-                         (str (inc i) ". " choice))
-                       choices)))
+            (map-indexed (fn [i choice]
+                           (str (inc i) ". " choice))
+                         choices)))
+
+(defn compile-votes [votes choices]
+  (zipmap choices
+          (reduce
+           #(update-in %1 [(second %2)] inc)
+           (into [] (repeat (count choices) 0))
+           votes)))
+
+
+(defn display-compiled-votes [votes]
+  (str/join "\n"
+            (map (fn [[choice number]]
+                   (str choice ": " number " vote" (if (not= number 1) "s")))
+                 votes)))
 
 
 ;; commands
+
 
 (defn handle-create [robot msg]
   (if (poll-active? robot)
@@ -45,6 +59,7 @@
 (defn handle-end [robot msg]
   (if (poll-active? robot)
     (do (brain-set robot :question nil)
+        (brain-set robot :votes {})
         (.send msg "The poll is now ended"))
     (.send msg "There isn't an active poll at the moment")))
 
@@ -52,14 +67,33 @@
 (defn handle-show [robot msg]
   (let [question (brain-get robot :question)
         choices (brain-get robot :choices)]
-    (if (nil? question)
-      (.send msg "There isn't an active poll at the moment")
-      (.send msg (str "Question: " question "\n"
-                      (display-choices choices))))))
+    (.send msg
+           (if (nil? question)
+             "No active poll"
+             (str "Question: " question "\n" (display-choices choices))))))
 
 
 (defn handle-vote [robot msg]
-  (.send msg (str "ok " (.. msg -message -user -name))))
+  (let [user-key (.toLowerCase (.. msg -message -user -name))
+        votes (brain-get robot :votes)
+        choices (brain-get robot :choices)
+        [_ schoice] (.-match msg)
+        choice (dec (js/Number schoice))]
+
+    (if (contains? votes user-key)
+      (.reply msg "You already voted for this poll")
+      (if (or (< choice 0) (> choice (count choices)))
+        (.reply msg "No such answer is available")
+        (do (brain-set robot :votes (assoc votes user-key choice))
+            (.reply msg "Your vote was registered"))))))
+
+
+(defn handle-results [robot msg]
+  (if (poll-active? robot)
+    (let [votes (brain-get robot :votes)
+          choices (brain-get robot :choices)]
+      (.send msg (display-compiled-votes (compile-votes votes choices))))
+    (.send msg "There isn't an active poll at the moment")))
 
 
 (defn hubot-main [robot]
@@ -72,12 +106,16 @@
             (partial handle-end robot))
 
   (.respond robot
-            #"poll (show|current)"
+            #"poll(\s(show|current))?"
             (partial handle-show robot))
 
   (.respond robot
-            #"poll vote"
-            (partial handle-vote robot)))
+            #"poll vote ([0-9]+)"
+            (partial handle-vote robot))
+
+  (.respond robot
+            #"poll results"
+            (partial handle-results robot)))
 
 
 ;;; exports
